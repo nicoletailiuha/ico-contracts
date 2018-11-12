f947a7a9bfd7f86ee65cb7f4115a79d753c1c303
Cristian Covali <cristian.covali@titanium-soft.com>
2018-11-12 13:28:59 +0200

    Emit "Allocate" event on token allocation

--- migrations/1537177537_b_d_p_crowdsale.js
+++ migrations/1537177537_b_d_p_crowdsale.js
@@ -1,28 +1,30 @@
 const BDPCrowdsale = artifacts.require('BDPCrowdsale');
 
-module.exports = async function(deployer) {
+module.exports = function(deployer) {
   const {
     WALLET_ADDRESS, ETH_TO_USD_RATE,
     ECOSYSTEM_ADDRESS, RESERVE_ADDRESS,
     TEAM_ADDRESS, ADVISORS_ADDRESSES, ADMIN_ADDRESSES
   } = process.env;
 
-  await deployer.deploy(
-    BDPCrowdsale,
-    WALLET_ADDRESS, // wallet
-    ETH_TO_USD_RATE, // rate
-  );
+  deployer.then(async () => {
+    await deployer.deploy(
+      BDPCrowdsale,
+      WALLET_ADDRESS, // wallet
+      ETH_TO_USD_RATE, // rate
+    );
 
-  const crowdsale = await BDPCrowdsale.deployed();
+    const crowdsale = await BDPCrowdsale.deployed();
 
-  await crowdsale.addAddressesToAdmins(ADMIN_ADDRESSES.split(','));
+    await crowdsale.addAddressesToAdmins(ADMIN_ADDRESSES.split(','));
 
-  await crowdsale.distributeInitialTokens(
-    ECOSYSTEM_ADDRESS, // ecosystemAddress
-    RESERVE_ADDRESS, // reserveAddress
-    TEAM_ADDRESS, // teamAddress
-    ADVISORS_ADDRESSES.split(','), // advisorsAddresses
-  );
+    await crowdsale.distributeInitialTokens(
+      ECOSYSTEM_ADDRESS, // ecosystemAddress
+      RESERVE_ADDRESS, // reserveAddress
+      TEAM_ADDRESS, // teamAddress
+      ADVISORS_ADDRESSES.split(','), // advisorsAddresses
+    );
 
-  await crowdsale.initializeSaleStages();
+    await crowdsale.initializeSaleStages();
+  });
 };
